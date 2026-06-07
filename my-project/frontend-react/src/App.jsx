1f26fab613da3f8f656145feb84414eb926a431b
cheddochi <crome81@naver.com>
2026-06-07 21:21:47 +0900

    Add route for AxProjectPage in App component

--- my-project/frontend-react/src/App.jsx
+++ my-project/frontend-react/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import MainPage from './pages/MainPage'
+import AxProjectPage from './pages/AxProjectPage'
 import EcommercePredictionApp from './ax_projects/ecommerce_prediction'
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/ax/ecommerce-prediction/*" element={<EcommercePredictionApp />} />
+      <Route path="/ax/:slug" element={<AxProjectPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
